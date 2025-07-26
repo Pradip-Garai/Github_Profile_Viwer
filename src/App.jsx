ec53642ef2b7c8004074e1b3db0f22cb75178949
Pradip-Garai <pradipgarai173@gmail.com>
2025-07-26 17:14:10 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './pages/Header'
-import Body from './pages/Body'
+import Header from './components/Header';
+import Body from './components/Body'
 
 function App() {
   return <>
